295a3efc78cf8ed95ba2e03cf89a8a8d45af0949
João Mangueira <jamjs7@gmail.com>
2021-01-08 13:29:23 -0300

    retirando a barra do final das rotas

--- src/Router.php
+++ src/Router.php
@@ -13,9 +13,20 @@ class Router
         $this->routes[$pattern] = $callback;
     }
 
-    public function run()
+    public function getCurrentUrl()
     {
         $url = $_SERVER['PATH_INFO'] ?? '/';
+
+        if(strlen($url) > 1) {
+            $url = rtrim($url, '/');
+        }
+
+        return $url;
+    }
+
+    public function run()
+    {
+        $url = $this->getCurrentUrl();
 
         foreach ($this->routes as $route => $action) {
             if(preg_match($route, $url, $params)) {
