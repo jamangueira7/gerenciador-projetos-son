946121ac9119eef0a115f3e7e661e0341bdb8cf4
João Mangueira <jamjs7@gmail.com>
2021-01-08 17:44:14 -0300

    trazendo dados do banco

--- config/routes.php
+++ config/routes.php
@@ -1,11 +1,13 @@
 <?php
 
-$router->add('GET','/', function () use ($container) {
-    $pdo = $container['db'];
-    var_dump($pdo);
+use App\Models\Users;
+
+$router->add('GET','/', function () {
     return 'Estamos na homepage';
 });
 
-$router->add('GET','/projects/(\d+)', function ($params) {
-    return 'Estamos listando o projetos de id: ' . $params[1];
+$router->add('GET','/projects/(\d+)', function ($params) use ($container) {
+    $user = new Users($container);
+    $data = $user->get($params[1]);
+    return 'Meu nome é  ' . $data['name'];
 });
