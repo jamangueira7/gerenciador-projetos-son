1b620754090d15303c7adffac1e39685e2201581
João Mangueira <jamjs7@gmail.com>
2021-01-08 18:41:52 -0300

    add httpFoundation

--- app/Controllers/UserController.php
+++ app/Controllers/UserController.php
@@ -6,10 +6,10 @@ use App\Models\Users;
 
 class UserController
 {
-    public function show($container, $params)
+    public function show($container, $request)
     {
         $user = new Users($container);
-        $data = $user->get($params[1]);
+        $data = $user->get($request->attributes->get(1));
         return 'Meu nome é  ' . $data['name'];
     }
 }
