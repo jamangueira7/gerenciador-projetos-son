cbea74bd1c391960cdd92ad7e3c94ea397ab0301
João Mangueira <jamjs7@gmail.com>
2021-01-09 13:24:02 -0300

    criando middlewares

--- bootstrap.php
+++ bootstrap.php
@@ -5,6 +5,7 @@ require __DIR__.'/vendor/autoload.php';
 $router = new \SON\Framework\Router;
 
 require __DIR__.'/config/containers.php';
+require __DIR__.'/config/middlewares.php';
 require __DIR__.'/config/events.php';
 require __DIR__.'/config/routes.php';
 
@@ -17,7 +18,16 @@ try {
       'params' => $result['params'],
 
     ];
+
+    foreach ($middlewares['before'] as $middleware) {
+        $middleware($container);
+    }
     $response($result['action'], $params);
+
+    foreach ($middlewares['after'] as $middleware) {
+        $middleware($container);
+    }
+
 } catch (\SON\Framework\Exceptions\HttpException $e) {
     echo json_encode(['error' => $e->getMessage()]);
 }
