4f3f4589e7c4bfa259164b24255450e802d96058
João Mangueira <jamjs7@gmail.com>
2021-01-10 11:44:39 -0300

    criando CRUD

--- app/src/Controllers/UserController.php
+++ app/src/Controllers/UserController.php
@@ -6,12 +6,31 @@ use App\Models\Users;
 
 class UserController
 {
+    public function index($container, $request)
+    {
+        return 'index';
+    }
+
+    public function create($container, $request)
+    {
+        return 'create';
+    }
+
     public function show($container, $request)
     {
-
         $user = new Users($container);
         $user->create(['name' => 'João']);
         $data = $user->get($request->attributes->get(1));
         return $data;
+    }
+
+    public function update($container, $request)
+    {
+        return 'update';
+    }
+
+    public function delete($container, $request)
+    {
+        return 'delete';
     }
 }
