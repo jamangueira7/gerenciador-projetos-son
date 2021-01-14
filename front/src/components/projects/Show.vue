2564de5816d33ce661c9f23d6bdf18e5765ce6e2
João Mangueira <jamjs7@gmail.com>
2021-01-14 20:29:34 -0300

    Criando componente detalhe do projeto

---
+++ front/src/components/projects/Show.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout row wrap>
+    <v-flex x12>
+      <h2 class="mb-2">Nome do projeto</h2>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+
+}
+</script>
