a80628ddd7cb3454b122df9ffbb2c0310c8aec80
João Mangueira <jamjs7@gmail.com>
2021-01-14 20:41:27 -0300

    Criando componente de sessão 1

--- front/src/components/sections/List.vue
+++ front/src/components/sections/List.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <p>Sections List</p>
-  </div>
+  <v-layout id="sections-container" row>
+    <v-flex xs3 v-for="n in 3">
+      <v-card color="blue-grey lighten-5">
+        <v-card-title primary-title class="blue-grey white--text">
+          <div class="headline">Sessão {{n}}</div>
+        </v-card-title>
+        <v-card-text>
+          adfadfsdf
+        </v-card-text>
+        <v-card-text>
+          // lista tarefas
+        </v-card-text>
+        <v-card-text>
+          // add tarefa
+        </v-card-text>
+      </v-card>
+    </v-flex>
+    <v-flex xs3>
+      //criar uma sessão
+    </v-flex>
+  </v-layout>
 </template>
