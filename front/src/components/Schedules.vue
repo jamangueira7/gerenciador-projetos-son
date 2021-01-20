a0b38c31e9f7fddefc8cb527c5b21ba77c98f332
João Mangueira <jamjs7@gmail.com>
2021-01-20 19:37:49 -0300

    configurando calentario

--- front/src/components/Schedules.vue
+++ front/src/components/Schedules.vue
@@ -3,13 +3,39 @@
     <v-flex xs12>
       <h2 class="display-1 mb-2">Agenda</h2>
     </v-flex>
+    <v-flex xs6>
+      <v-date-picker
+        v-model="date"
+        locale="pt-br"
+        landscape
+        full-width
+        color="green lighten-1"
+      ></v-date-picker>
+
+      <v-btn @click="goToToday()">Centralizar</v-btn>
+    </v-flex>
+    <v-flex xs6>
+      <h2 class="display-1 mb-2">Agenda</h2>
+    </v-flex>
   </v-layout>
 </template>
 
 <script>
 export default {
   data() {
+    return {
+      date: null
+    }
+  },
+  methods: {
+    goToToday() {
+      this.date = null;
 
+      setTimeout(() => {
+        const date = new Date();
+        this.date = date.toISOString().substr(0,10);
+      }, 500);
+    }
   },
   mounted() {
     this.$store.dispatch('projects/getAll');
