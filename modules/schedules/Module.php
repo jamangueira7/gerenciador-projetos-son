90c41968cef4060a19d4d4cc7d4ba07c24926266
João Mangueira <jamjs7@gmail.com>
2021-01-20 20:23:56 -0300

    Adicionando o novo modulo Schedules no backend

---
+++ modules/schedules/Module.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace SON\Framework\Tasks;
+
+use SON\Framework\Modules\IContract;
+
+class Module implements IContract
+{
+    public function getNamespace() : array
+    {
+        return [
+            'SON\\Framework\\Schedules\\' => __DIR__ . '/src'
+        ];
+    }
+
+    public function getContainerConfig() :string
+    {
+        return __DIR__.'/config/containers.php';
+    }
+
+    public function getEventConfig() :string
+    {
+        return __DIR__.'/config/events.php';
+    }
+
+    public function getMiddlewareConfig() :string
+    {
+        return __DIR__.'/config/middlewares.php';
+    }
+
+    public function getRouteConfig() :string
+    {
+        return __DIR__.'/config/routes.php';
+    }
+}
---
+++ modules/schedules/src/Controllers/SchedulesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SON\Framework\Schedules\Controllers;
+
+use SON\Framework\Controller;
+
+class SchedulesController extends Controller
+{
+    protected function getModel(): string
+    {
+        return 'schedules_model';
+    }
+}
